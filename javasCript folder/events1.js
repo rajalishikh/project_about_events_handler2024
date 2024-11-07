cb81770486a071ddffb89a63be64a6a58a096bbb
unknown <nahidujjamanraj@gmail.com>
2024-11-07 12:44:01 +0600

    Know about most important part of how to add event listner

--- javasCript folder/events1.js
+++ javasCript folder/events1.js
@@ -13,3 +13,15 @@ makeBlue.onclick = colorChange;
 function colorChange() {
   document.body.style.backgroundColor = "blue";
 };
+
+// step5
+let Make_orange=document.getElementById('OrangeMake')
+Make_orange.addEventListener('click',function change(){
+    document.body.style.backgroundColor='orange'
+})
+
+// step5
+
+document.getElementById('Make_pink').addEventListener('click',function(){
+    document.body.style.backgroundColor='pink'
+})
