97b996fef21aed14f2d3421d60d150b958eeeb9b
unknown <nahidujjamanraj@gmail.com>
2024-11-07 12:21:25 +0600

    Know about onclick 3 different methods

--- javasCript folder/events1.js
+++ javasCript folder/events1.js
@@ -1,1 +1,15 @@
-console.log('Events de re baba ')
+// step2
+
+function make_red() {
+  document.body.style.backgroundColor = "red";
+}
+
+// step3
+let makeBlue = document.getElementById("makeBlue");
+
+makeBlue.onclick = colorChange;
+
+
+function colorChange() {
+  document.body.style.backgroundColor = "blue";
+};
