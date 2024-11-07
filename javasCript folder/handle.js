828798eabce5395f2d6d6aa23491d9997be1ad66
unknown <nahidujjamanraj@gmail.com>
2024-11-07 21:05:56 +0600

    In this part i can change a text by the input text

--- javasCript folder/handle.js
+++ javasCript folder/handle.js
@@ -12,3 +12,13 @@ document.getElementById('addeventListner').addEventListener('click',function(){
     let change_text_addEventListener=document.getElementById("Change_22")
     change_text_addEventListener.innerText='change by addEventListener '
 })
+
+// step3
+document.getElementById('cange_input').addEventListener('click',function(){
+    let input_text=document.getElementById('input_field_text')
+    let input_value=input_text.value;
+
+    let headingText=document.getElementById('change_hading')
+    headingText.innerText=input_value;
+    
+})
