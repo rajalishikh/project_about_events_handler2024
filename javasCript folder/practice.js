8edee0f4732e609c03fedfd3123ffc1f2b7bd111
unknown <nahidujjamanraj@gmail.com>
2024-11-11 09:18:47 +0600

    In this program I make comment section

---
+++ javasCript folder/practice.js
@@ -0,0 +1,14 @@
+document.getElementById('comment_button').addEventListener('click',function(){
+   let text_input=document.getElementById('text_input')
+   let text_input_value=text_input.value;
+
+//    add in comment 
+let comment_section=document.getElementById('comment section');
+let create_p=document.createElement('p')
+console.log(create_p)
+create_p.innerText= text_input_value
+comment_section.appendChild(create_p)
+
+text_input.value='';
+   
+});
