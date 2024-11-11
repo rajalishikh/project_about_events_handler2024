055849d1d86ef60d2939d81f57cbf9d9536d1214
unknown <nahidujjamanraj@gmail.com>
2024-11-11 09:46:16 +0600

    I just tring to remove  a child but i cant finish it

---
+++ javasCript folder/practice2.js
@@ -0,0 +1,5 @@
+document.getElementById('delete_section').addEventListener('click',function(event){
+    console.log('delete')
+    let comment_section=document.getElementsByClassName('comment')
+    
+})
