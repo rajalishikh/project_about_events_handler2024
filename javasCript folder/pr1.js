edb5a69e5b7bd7c5f6d258f07294ac812009b856
unknown <nahidujjamanraj@gmail.com>
2024-11-16 11:49:56 +0600

    In this program i make a function that add number

---
+++ javasCript folder/pr1.js
@@ -0,0 +1,15 @@
+let sum=0;
+// my add count work 
+function click1(){
+   let add=document.getElementById('count_number')
+   
+   sum=sum+1;
+   add.innerText=sum
+   
+
+
+}
+
+document.getElementById('button_2').addEventListener('click',function(){
+    
+})
